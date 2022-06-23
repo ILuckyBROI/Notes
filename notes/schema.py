cde836027ce104d02684f46bb0b7bd0be4dd338f
ILuckyBROI <QalQ1@bk.ru>
2022-06-23 05:26:01 -0700

    10

---
+++ notes/schema.py
@@ -0,0 +1,39 @@
+import graphene
+from graphene_django import DjangoObjectType
+from TODO.models import User, Project, Todo
+
+
+class UserType(DjangoObjectType):
+    class Meta:
+        model = User
+        fields = '__all__'
+
+
+class ProjectType(DjangoObjectType):
+    class Meta:
+        model = Project
+        fields = '__all__'
+
+
+class TodoType(DjangoObjectType):
+    class Meta:
+        model = Todo
+        fields = '__all__'
+
+
+class Query(graphene.ObjectType):
+    all_users = graphene.List(UserType)
+    all_projects = graphene.List(ProjectType)
+    all_tods = graphene.List(TodoType)
+
+    def resolve_all_users(self, info):
+        return User.objects.all()
+
+    def resolve_all_projects(self, info):
+        return Project.objects.all()
+
+    def resolve_all_tods(self, info):
+        return Todo.objects.all()
+
+
+schema = graphene.Schema(query=Query)
